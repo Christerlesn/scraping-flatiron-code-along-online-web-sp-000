ae29ccb6abe2db8dcfe547d09e687ad669780356
Christerle Nounoute <christerlesn@gmail.com>
2020-10-24 02:35:37 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,6 +24,18 @@ class Scraper
     doc.css(".post")
   end
 
+  def make_courses
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+    doc.css(".post").each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+
+  end
+
   def self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
